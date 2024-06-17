f72d9058002bc5d486db210894a1385930b18aea
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2024-06-17 19:07:41 +0300

    feat: update  products page

---
+++ src/app/(pages)/products/page.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { Metadata } from 'next'
+import { draftMode } from 'next/headers'
+
+import { Category, Page } from '../../../payload/payload-types'
+import { fetchDoc } from '../../_api/fetchDoc'
+import { fetchDocs } from '../../_api/fetchDocs'
+import { Blocks } from '../../_components/Blocks'
+import { Gutter } from '../../_components/Gutter'
+import { HR } from '../../_components/HR'
+import { generateMeta } from '../../_utilities/generateMeta'
+import Filters from './Filters'
+
+import classes from './index.module.scss'
+
+const Products = async () => {
+  const { isEnabled: isDraftMode } = draftMode()
+  let page: Page | null = null
+  let categories: Category[] | null = null
+
+  try {
+    page = await fetchDoc<Page>({ collection: 'pages', slug: 'products', draft: isDraftMode })
+    categories = await fetchDocs<Category>('categories')
+  } catch (error) {
+    console.log(error)
+  }
+  return (
+    <div className={classes.container}>
+      <Gutter className={classes.products}>
+        <Filters categories={categories} />
+        <Blocks blocks={page.layout} disableTopPadding={true} />
+      </Gutter>
+      <HR />
+    </div>
+  )
+}
+
+export default Products
+
+export async function generateMetadata({ params: { slug } }): Promise<Metadata> {
+  const { isEnabled: isDraftMode } = draftMode()
+
+  let page: Page | null = null
+
+  try {
+    page = await fetchDoc<Page>({
+      collection: 'pages',
+      slug: 'products',
+      draft: isDraftMode,
+    })
+  } catch (error) {}
+
+  return generateMeta({ doc: page })
+}
